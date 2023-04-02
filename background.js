32f24c1eae3c9445fa6e9e53cbcdb9ba669f4f72
barathrum54 <tahabdurmus0@gmail.com>
2023-04-02 21:55:28 +0300

    ai change

--- background.js
+++ background.js
@@ -3,64 +3,53 @@ let intervalId;
 let currentTime = 25 * 60;
 let pauseTabId;
 
-chrome.action.onClicked.addListener((tab) => {
-    if (isRunning) {
-        clearInterval(intervalId);
+chrome.action.onClicked.addListener(handlePomodoroClick);
+chrome.tabs.onRemoved.addListener(handlePauseTabRemove);
+chrome.runtime.onMessage.addListener(handlePauseMessage);
 
-        // Open pause.html and show a pause notification
-        chrome.tabs.create({ url: 'pause.html' }, (newTab) => {
-            pauseTabId = newTab.id;
-        });
-        showPauseNotification();
-        isRunning = !isRunning;
-        updateBadge();
+function handlePomodoroClick(tab) {
+    if (isRunning) {
+        pausePomodoro();
     } else {
-        if (pauseTabId) {
-            chrome.tabs.get(pauseTabId, (tab) => {
-                if (chrome.runtime.lastError) {
-                    // The tab doesn't exist anymore, so it's safe to start the timer
-                    intervalId = setInterval(updateTimer, 1000);
-                    isRunning = !isRunning;
-                    updateBadge();
-                } else {
-                    // The pause tab still exists, so focus on it instead of starting the timer
-                    chrome.tabs.update(pauseTabId, { active: true });
-                }
-            });
-        } else {
-            intervalId = setInterval(updateTimer, 1000);
-            isRunning = !isRunning;
-            updateBadge();
-        }
+        startPomodoro();
     }
-});
-chrome.tabs.onRemoved.addListener((tabId) => {
+}
+
+function pausePomodoro() {
+    clearInterval(intervalId);
+    chrome.tabs.create({ url: 'pause.html' }, handlePauseTabCreated);
+
+    showNotification('Pomodoro Durduruldu', 'Pomodoro zamanlayıcısı durduruldu.');
+    isRunning = false;
+    updateBadge();
+}
+
+function handlePauseTabCreated(newTab) {
+    pauseTabId = newTab.id;
+}
+
+function handlePauseTabRemove(tabId) {
     if (tabId === pauseTabId) {
         pauseTabId = null;
     }
-});
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+}
+
+function handlePauseMessage(request, sender, sendResponse) {
     if (request.type === 'pauseMessage') {
         const message = request.message;
-        showPauseNotification(message);
+        showNotification('Duraklatıldı', message);
     }
-});
-function showPauseNotification() {
-    chrome.notifications.create({
-        type: 'basic',
-        iconUrl: chrome.runtime.getURL('icon128.png'),
-        title: 'Pomodoro Durduruldu',
-        message: 'Pomodoro zamanlayıcısı durduruldu.'
-    });
+    if (request.type === 'errorMessage') {
+        showNotification('Hata', 'Durdurma sekmesi hala açık.');
+    }
 }
-function showPomodoroCompleteNotification() {
-    chrome.notifications.create({
-        type: 'basic',
-        iconUrl: chrome.runtime.getURL('icon128.png'),
-        title: 'Pomodoro Tamamlandı',
-        message: 'Bir Pomodoro tamamlandı. Şimdi mola zamanı!'
-    });
+
+function startPomodoro() {
+    intervalId = setInterval(updateTimer, 1000);
+    isRunning = true;
+    updateBadge();
 }
+
 function updateTimer() {
     if (currentTime <= 0) {
         clearInterval(intervalId);
@@ -74,17 +63,24 @@ function updateTimer() {
     updateBadge();
 }
 
+function showNotification(title, message) {
+    chrome.notifications.create({
+        type: 'basic',
+        iconUrl: chrome.runtime.getURL('favicon-128.png'),
+        title,
+        message
+    });
+}
+
+function showPomodoroCompleteNotification() {
+    showNotification('Pomodoro Tamamlandı', 'Bir Pomodoro tamamlandı. Şimdi mola zamanı!');
+}
+
 function updateBadge() {
     chrome.action.setBadgeBackgroundColor({ color: isRunning ? "green" : "red" });
     chrome.action.setBadgeTextColor({ color: "white" });
 
-    if (!isRunning) {
-        let pauseText = "-" + formatTime(currentTime).toString()
-        chrome.action.setBadgeText({ text: pauseText });
-    } else {
-        chrome.action.setBadgeText({ text: formatTime(currentTime).toString() });
-
-    }
+    chrome.action.setBadgeText({ text: formatTime(currentTime).toString() });
 }
 
 function formatTime(seconds) {
@@ -96,16 +92,16 @@ function formatTime(seconds) {
 function updatePomodoroHistory() {
     const today = new Date().toISOString().split("T")[0];
 
-    chrome.storage.local.get("pomodoroHistory", (data) => {
-        let pomodoroHistory = data.pomodoroHistory || [];
+    chrome.storage.local.get("pomodoroHistory", ({ pomodoroHistory }) => {
+        let updatedPomodoroHistory = pomodoroHistory || [];
 
-        const existingEntry = pomodoroHistory.find((entry) => entry.date === today);
+        const existingEntry = updatedPomodoroHistory.find((entry) => entry.date === today);
         if (existingEntry) {
             existingEntry.pomodoros++;
         } else {
-            pomodoroHistory.push({ date: today, pomodoros: 1 });
+            updatedPomodoroHistory.push({ date: today, pomodoros: 1 });
         }
 
-        chrome.storage.local.set({ pomodoroHistory });
+        chrome.storage.local.set({ pomodoroHistory: updatedPomodoroHistory });
     });
 }
